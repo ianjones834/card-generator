d887079968ebec4a1d0be49fed736039a4f7236a
Ian Jones <ianjones834@protonmail.com>
2025-12-11 21:36:03 -0500

    added EditorJS

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
-let cards = [];
+import EditorJS from './node_modules/@editorjs/editorjs/dist/editorjs.mjs';
+import EditorjsList from './node_modules/@editorjs/list/dist/editorjs-list.mjs';
+import Header from './node_modules/@editorjs/header/dist/header.mjs';
 
-function newBlankCard() {
+let cards = [];
+let editors = [];
+
+function newEditor(id) {
+  return new EditorJS({
+    holder: `editor-${id}`,
+    tools: {
+      header: {
+        class: Header,
+        config: {
+          placeholder: 'Enter a header',
+          levels: [3,5],
+          defaultLevel: 3
+        },
+      },
+      list: EditorjsList,
+    }
+  })
+}
+
+function createEditors() {
+  for (let card of cards) {
+    editors.push(newEditor(card.id))
+  }
+}
+
+export function newBlankCard() {
   return {
     id: cards.length,
     title: '',
     sub_title: '',
-    description: ''
+    description: '',
+    editor: `editor-${cards.length}`
   }
 }
 
-function resetCardList() {
+export function resetCardList() {
   clearCardList();
   addCardsToHtml();
 }
 
-function addCardToHtml(card) {
-  const card_html = `<div class="card" onclick="editCards(${card.id});">
+export function addCardToHtml(card) {
+  const card_html = `<div class="card" id="editor-${card.id}">
     <h3>${card.title}</h3>
     <h5>${card.sub_title}</h5>
     ${card.description}
   </div>`;
 
   $('#card-list').append(card_html);
+
 }
 
-function editCards(id) {
+export function editCards(id) {
   cards[id] = cardPrompt(id);
   resetCardList();
 }
 
-function cardPrompt(id) {
+export function cardPrompt(id) {
   let title = prompt('Enter title', '');
   let sub_title = prompt('Enter Subtitle', '');
   let description = prompt('Enter Description', '');
@@ -41,6 +71,8 @@ function addCardsToHtml() {
   for (let card of cards) {
     addCardToHtml(card);
   }
+
+  createEditors()
 }
 
 function clearCardList() {
